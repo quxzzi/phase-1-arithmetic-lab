002e22238fdc828e9ef82e6ee6661583fdbbc591
Kevin Dean <kdean1011@gmail.com>
2023-04-28 19:45:13 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31, num2 = 2, multiply =num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 4, num4 = 16;
+const mod = num3 % num4;
+
+const numbers = [5, 12, 20, 3, 8];
+let max = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers[i];
+  }
+}
